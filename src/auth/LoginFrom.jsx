f62d9edf8f9b7862184bb0147fc858b89ea78721
upomathasin <tahsinchowdhuryupoma@gmail.com>
2024-03-04 20:22:44 +0600

    login form created

--- src/auth/LoginFrom.jsx
+++ src/auth/LoginFrom.jsx
@@ -1,5 +1,50 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import Field from "../common/Field";
 
 export default function LoginFrom() {
-  return <div>LoginFrom</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const submitFrom = (formData) => {
+    console.log("From Data : ", formData);
+  };
+  return (
+    <div>
+      <form
+        onSubmit={handleSubmit(submitFrom)}
+        className="border-b border-[#3F3F3F] pb-10 lg:pb-[60px]"
+      >
+        <Field label="Email">
+          <input
+            className={`auth-input ${
+              !!errors.email ? "border-red-500" : "border-gray-200"
+            }`}
+            {...register("email", { required: "Email id is required" })}
+            type="email"
+            name="email"
+          />
+        </Field>
+        <Field label="Password">
+          <input
+            className={`auth-input ${
+              !!errors.password ? "border-red-500" : "border-gray-200"
+            }`}
+            {...register("password", { required: "Email id is required" })}
+            type="password"
+            name="password"
+          />
+        </Field>
+        <button
+          class="auth-input bg-lwsGreen font-bold text-deepDark transition-all hover:opacity-90"
+          type="submit"
+        >
+          Login
+        </button>
+      </form>
+    </div>
+  );
 }
--- src/common/Field.jsx
+++ src/common/Field.jsx
@@ -1,5 +1,26 @@
 import React from "react";
 
-export default function Field() {
-  return <div>Field</div>;
+const getChildId = (children) => {
+  const child = React.Children.only(children);
+  if ("id" in child?.props) {
+    return child.props.id;
+  }
+};
+export default function Field({ label, error, children, htmlFor }) {
+  const id = htmlFor || getChildId(children);
+  return (
+    <div className=" form-control">
+      {label && (
+        <label htmlFor={id} className="auth-label">
+          {label}
+        </label>
+      )}
+      {children}
+      {!!error && (
+        <div className="text-red-400" role="alert">
+          {error.message}
+        </div>
+      )}
+    </div>
+  );
 }
